66249d6a9e40d9f22d62ae08e88c78b80a3d90c7
BenWesterhof <weste2br@cmich.edu>
2026-04-20 13:41:10 -0400

    added Java

---
+++ Lab14-recap/JS/script.js
@@ -0,0 +1,12 @@
+console.log("JavaScript is Connected!");
+
+
+// Open the overlay menu//
+function openOverlay() {
+    document.getElementById("overlay-menu").style.width = "100vw";
+}
+
+// Close the overlay menu//
+function closeOverlay() {
+    document.getElementById("overlay-menu").style.width = "0vw";
+}
